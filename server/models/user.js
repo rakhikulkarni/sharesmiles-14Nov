7bde588c5ccb5ab390221a1c75cef321c939dade
rakhikulkarni <rakhikulkarni3105@gmail.com>
2023-12-02 16:23:43 -0500

    model and route files

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+    user_id : '123', 
+    username : 'rak1',
+    email : 'rak1@gmail.com'
+    },
+    {
+     user_id : '876', 
+     username : 'rak2',
+     email : 'rak2@gmail.com'
+    },
+    {
+    user_id : '345', 
+    username : 'rak3',
+    email : 'rak3@gmail.com'
+    },
+
+];
+
+let getUsers = () => users;
+module.exports ={getUsers};
